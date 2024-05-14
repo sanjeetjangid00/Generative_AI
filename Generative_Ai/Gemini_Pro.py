c1c4c10f181db7cf05b140cad98e311a619f23ab
sanjeetjangid00 <167337847+sanjeetjangid00@users.noreply.github.com>
2024-05-14 23:10:53 +0530

    Update Gemini_Pro.py

--- Generative_Ai/Gemini_Pro.py
+++ Generative_Ai/Gemini_Pro.py
@@ -8,7 +8,7 @@ def centered_content():
 # Center-align the title
 with centered_content()[1]:
     st.title(":blue[G]:red[o]:green[o]:blue[g]:green[l]:red[e] :blue[G]:red[e]:green[m]:blue[i]:green[n]:red[i] :blue[P]:red[r]:green[o]")
-    st.spinner("Thinking")
+    
 chat=st.chat_input("Ask Something...",key=2003,disabled=False)
 api_key="AIzaSyC_L3-d181ibSultwSEuGm6P4XwE8HIsEQ"
 genai.configure(api_key=api_key)
@@ -16,6 +16,7 @@ model=genai.GenerativeModel("gemini-pro")
 if chat:
     response=model.generate_content(chat)
     st.write("User :adult: :", chat)
+    st.spinner("Thinking")
     st.write("Gemini :robot_face: :", response.text)
     #st.sidebar.text_area("Human :adult:", value=chat, height=100, max_chars=None)
     #st.sidebar.text_area("ChatGpt :robot_face:", value=response.text, height=200, max_chars=None)
