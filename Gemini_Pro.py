c04293be19ba40169b9ef6f30c2d49d7efa9635c
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2024-09-16 00:05:43 +0530

    Update Gemini_Pro.py
    code update

--- Gemini_Pro.py
+++ Gemini_Pro.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 import streamlit as st
-
+import time
 def centered_content():
     col1, col2, col3 = st.columns([1, 3, 1])
     return col1, col2, col3
@@ -13,13 +13,22 @@ chat=st.chat_input("Ask Something...",key=2003,disabled=False)
 api_key="AIzaSyB9i0slQTLC_5DZ29mbj1esnmtqdm1NfJg"
 genai.configure(api_key=api_key)
 model=genai.GenerativeModel("gemini-pro")
+
+def chatting(chat):
+    return model.generate_content(chat)
+def stream(response):
+            st.write(":green[Gemini] :robot_face: :")
+            for word in response.text.split(" "):
+                yield word + " "
+                time.sleep(0.02)
 if chat:
-    with st.spinner("Thinking...."):
-        response=model.generate_content(chat)
-        st.write(":blue[User] :adult: :", chat)
-        st.write(":green[Gemini] :robot_face: :", response.text)
-        #st.sidebar.text_area("Human :adult:", value=chat, height=100, max_chars=None)
-        #st.sidebar.text_area("ChatGpt :robot_face:", value=response.text, height=200, max_chars=None)
+    st.write(":blue[User] :adult: :", chat)
+    with st.spinner("Generating...."):
+        #st.write(":green[Gemini] :robot_face: :")
+        response = chatting(chat)
+        st.write_stream(stream(response))
+
 else:
     st.warning("Please type something to generate content.")
     st.write(":blue[Gemini] :robot_face: : :green[Hi ! How can I assist you today?]")
+    st.write_stream()
