10e899560ddaf619ef7c685ee2f53cb0b49252b7
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2024-09-24 10:19:40 +0530

    Update Gemini_Pro.py

--- Gemini_Pro.py
+++ Gemini_Pro.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import streamlit as st
 import time
 def centered_content():
-    col1, col2, col3 = st.columns([1, 4, 2])
+    col1, col2, col3 = st.columns([1, 12, 1])
     return col1, col2, col3
 
 # Center-align the title
